5545bec89af4ad6dcae862c9476b536f4bd9bf91
Nicole Griffith <noteworthynicole@users.noreply.github.com>
2019-04-25 17:23:20 -0700

    Double time added

---
+++ src/logic/DoubleTimes.java
@@ -0,0 +1,64 @@
+package logic;
+
+import java.time.LocalTime;
+
+public class DoubleTimes {
+	private Times lecTimes;
+	private Times labTimes;
+	
+	public DoubleTimes(String day, String startTime, String endTime) {
+		this.lecTimes = new Times(day, startTime, endTime);
+	}
+	
+	public Times getLecTimes() {
+		return lecTimes;
+	}
+	
+	public Times getLabTimes() {
+		return labTimes;
+	}
+	
+	public String getLecDay() {
+		return lecTimes.getDay();
+	}
+	
+	public LocalTime getLecStartTime() {
+		return lecTimes.getStartTime();
+	}
+	
+	public LocalTime getLecEndTime() {
+		return lecTimes.getEndTime();
+	}
+	
+	@Override
+	   public String toString() {
+	      return "DoubleTimes Lec: " + lecTimes + " Lab: " + labTimes;
+	   }
+	
+	@Override
+	public boolean equals(Object other) {
+		if(other instanceof DoubleTimes && this.lecTimes.equals(((DoubleTimes) other).getLecTimes())) {
+			if(this.labTimes != null) {
+				return this.labTimes.equals(((DoubleTimes) other).getLabTimes());
+			}
+			return this.labTimes == null && (((DoubleTimes) other).getLabTimes()) == null;
+		}
+		return false;
+	}
+	
+	public void setLabTime(Times time) {
+		this.labTimes = time;
+	}
+	
+	@Override 
+	public int hashCode() {
+        int result = 31;
+        result = result * lecTimes.hashCode();
+        if(labTimes != null) {
+        	result = result * labTimes.hashCode();
+        }
+        return result;
+    }
+	
+	
+}
