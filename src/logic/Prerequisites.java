fa5646e237bdd0a3fc0063b6ab8c255851680d02
Nicole Griffith <noteworthynicole@users.noreply.github.com>
2019-05-14 18:04:47 -0700

    I just want this class to be committed please

--- src/logic/Prerequisites.java
+++ src/logic/Prerequisites.java
@@ -1,5 +1,15 @@
 package logic;
 
-public class Prerequisites {
+import org.mvel2.MVEL;
 
+public class Prerequisites {
+	
+	public static void main(String[] args) {
+		System.out.println(MVEL.eval("((true && false) || true) && true"));
+	}
+	
+	//for each class, access their prerequisites
+	//go to the user's account and access their list of prerequisites
+	//replace the class with either true or false depending on if the user has taken the class
+	//remove classes that don't meet prerequisites
 }
