031158ab88aac6f1b20d93bd1700daedf454a009
Roberto Dominguez <Dominguez7777@gmail.com>
2019-05-29 11:16:48 -0700

    Revert "fixed a test case"
    This reverts commit 7a0a383b8eb4be103c17619d42ee74510443645f.

---
+++ bin/src/components/Nav/TopBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TopBar = () => {
+    return (
+        <div className='header'>
+            <div className='logo'> Schedulsaur </div>
+        </div>
+    )
+}
+
+
+export default TopBar;
--- src/backend/test/TestDatabaseWrite.java
+++ src/backend/test/TestDatabaseWrite.java
@@ -12,32 +12,24 @@ import java.sql.Statement;
 import java.util.*;
 import java.util.logging.*;
 
-import logic.Class;
 import logic.Database;
 
 public class TestDatabaseWrite {
 	
-	private Class testClass1 = new Class("CSC 309");
-	private String encryptedPW1 = "gvznyfoyzhzfi";
-	private String dbPW1 = Database.mostSecureEncryptionEver(encryptedPW1);
-	private String dbURL1 = "jdbc:mysql://schedulsaur-database.coiryrpvj04m.us-west-1.rds.amazonaws.com?useSSL=false";
-	private String dbUsername1 = "schedulsaur";
-	private Logger logger1 = Logger.getLogger("Database");
-	
-	@Test
-	public void testWriteStudent() {
-		Database db = new Database();
-		Statement stmt = null;
-			try (Connection conn = DriverManager.getConnection(dbURL1,dbUsername1,dbPW1)){
+	 @Test
+	   public void testWriteStudent() {
+	      Database db = new Database();
+	      Statement stmt = null;
+			try (Connection conn = DriverManager.getConnection(DBSITE,SCHEDELSAUR,mostSecureEncryptionEver(ENCRYPTEDPW))){
 				stmt = conn.createStatement();
 				// calls go here
 				stmt.close();
 			} catch(SQLException se) {
 				//Handle errors for JDBC
-				logger1.log(Level.WARNING, se.toString());
+				logger.log(Level.WARNING, se.toString());
 			} catch(Exception e) {
 				//Handle errors for Class.forName
-				logger1.log(Level.WARNING, e.toString());
-		}
-	}
+				logger.log(Level.WARNING, e.toString());
+	   }
+
 }
