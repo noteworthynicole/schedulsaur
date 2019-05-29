6144241ce440d9b114d63c1e3e099962d8c318b9
Nicole Griffith <noteworthynicole@users.noreply.github.com>
2019-05-29 10:57:48 -0700

    Database test is broken

--- src/backend/test/TestDatabaseWrite.java
+++ src/backend/test/TestDatabaseWrite.java
@@ -4,12 +4,32 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
+import java.util.logging.*;
+
+import logic.Database;
+
 public class TestDatabaseWrite {
 	
-	@Test
-	public void test1() {
-		//placeholder tests
-		assertTrue(true);
-	}
+	 @Test
+	   public void testWriteStudent() {
+	      Database db = new Database();
+	      Statement stmt = null;
+			try (Connection conn = DriverManager.getConnection(DBSITE,SCHEDELSAUR,mostSecureEncryptionEver(ENCRYPTEDPW))){
+				stmt = conn.createStatement();
+				// calls go here
+				stmt.close();
+			} catch(SQLException se) {
+				//Handle errors for JDBC
+				logger.log(Level.WARNING, se.toString());
+			} catch(Exception e) {
+				//Handle errors for Class.forName
+				logger.log(Level.WARNING, e.toString());
+	   }
 
 }
