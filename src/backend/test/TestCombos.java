6432daa319a50dc88ebbc04443b036755ba14206
Jack Kooley <jkooley@gmail.com>
2019-06-03 10:12:54 -0700

    updated combos tests

--- src/backend/test/TestCombos.java
+++ src/backend/test/TestCombos.java
@@ -8,29 +8,20 @@ import logic.*;
 
 public class TestCombos {
 	
-	String mwf = "MWF";
-	String tr = "TR";
-	String mtrf = "MWRF";
-	String one = "01:00 PM";
-	String two = "02:00 PM";
-	String seven = "07:10 AM";
-	String eight = "08:10 AM";
-	String nine = "09:00 AM";
-	String ten = "10:10 AM";
-	String eleven = "11:10 AM";
+	private String mwf = "MWF";
+	private String tr = "TR";
+	private String seven = "07:10 AM";
+	private String eight = "08:10 AM";
+	private String nine = "09:00 AM";
 	
-	DoubleTimes time1 = new DoubleTimes(mwf, seven, nine);
-	DoubleTimes time2 = new DoubleTimes(mwf, eight, nine);
-	DoubleTimes time3 = new DoubleTimes(tr, eight, nine);
-	DoubleTimes time4 = new DoubleTimes(tr, ten, eleven);
-	DoubleTimes time5 = new DoubleTimes(mwf, ten, eleven);
-	DoubleTimes time6 = new DoubleTimes(mtrf, eight, eleven);
-	DoubleTimes time7 = new DoubleTimes(tr, one, two);
+	private DoubleTimes time1 = new DoubleTimes(mwf, seven, nine);
+	private DoubleTimes time2 = new DoubleTimes(mwf, eight, nine);
+	private DoubleTimes time3 = new DoubleTimes(tr, eight, nine);
 	
-	String[] line1 = new String[] {"CSC-309-02-2194", "6799", "Lab", "Falessi, D", "MWF", "11:10 AM", "12:00 PM", "N/A", "27", "25", "2", "3"};
-	String[] line2 = new String[] {"CSC-309-02-2195", "6798", "Lab", "Falessi, D", "MWF", "01:10 PM", "02:00 PM", "N/A", "27", "25", "2", "3"};
-	String[] line3 = new String[] {"CSC-308-02-2196", "6899", "Lab", "Falessi, D", "MWF", "11:10 AM", "12:00 PM", "N/A", "27", "25", "2", "3"};
-	String[] line4 = new String[] {"CSC-308-02-2197", "6398", "Lab", "Falessi, D", "MWF", "01:10 PM", "02:00 PM", "N/A", "27", "25", "2", "3"};
+	private String[] line1 = new String[] {"CSC-309-02-2194", "6799", "Lab", "Falessi, D", "MWF", "11:10 AM", "12:00 PM", "N/A", "27", "25", "2", "3"};
+	private String[] line2 = new String[] {"CSC-309-02-2195", "6798", "Lab", "Falessi, D", "MWF", "01:10 PM", "02:00 PM", "N/A", "27", "25", "2", "3"};
+	private String[] line3 = new String[] {"CSC-308-02-2196", "6899", "Lab", "Falessi, D", "MWF", "11:10 AM", "12:00 PM", "N/A", "27", "25", "2", "3"};
+	private String[] line4 = new String[] {"CSC-308-02-2197", "6398", "Lab", "Falessi, D", "MWF", "01:10 PM", "02:00 PM", "N/A", "27", "25", "2", "3"};
 	
 	@Test
 	public void testCombo1() {
@@ -48,6 +39,24 @@ public class TestCombos {
 		section2.add(sec4);
 		sections.add(section2);
 		List<List<Section>> output = GenerateSchedules.getCombos(sections, 0);
+		assertTrue(output.size() == 4);
+	}
+	
+	@Test
+	public void testCombo2() {
+		Section sec1 = GenerateSchedules.createSection(line1);
+		Section sec2 = GenerateSchedules.createSection(line2);
+		Section sec3 = GenerateSchedules.createSection(line3);
+		Section sec4 = GenerateSchedules.createSection(line4);
+		List<List<Section>> sections = new ArrayList<>();
+		List<Section> section1 = new ArrayList<>();
+		section1.add(sec1);
+		section1.add(sec2);
+		section1.add(sec3);
+		section1.add(sec4);
+		sections.add(section1);
+		List<List<Section>> output = GenerateSchedules.getCombos(sections, 0);
+		System.out.println(output.size());
 		assertTrue(output.size() == 4);
 	}
 	
