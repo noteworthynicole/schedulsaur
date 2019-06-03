167b7a89425c68dac1430eb7ba64a1c8bb11118e
Nicole Griffith <noteworthynicole@users.noreply.github.com>
2019-06-03 10:16:34 -0700

    All the test suites are done ... WriteDatabaseTests where are you?

--- src/backend/test/AllTests.java
+++ src/backend/test/AllTests.java
@@ -5,7 +5,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({TestTimeSuite.class, TestDatabaseSuite.class, TestCourseSuite.class, TestPrereqSuite.class, TestLoop.class })
+@SuiteClasses({TestTimeSuite.class, TestDatabaseSuite.class, TestCourseSuite.class, TestPrereqSuite.class, 
+	TestSchedStuffSuite.class, TestGenerateSuite.class, TestLoop.class })
 
 public class AllTests {
 
--- src/backend/test/TestGenerateSuite.java
+++ src/backend/test/TestGenerateSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestGenerateSchedules.class })
+@SuiteClasses({ TestGenerateSchedules.class, TestCombos.class })
 public class TestGenerateSuite {
 
 }
