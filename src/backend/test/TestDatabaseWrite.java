d8d25b52107ad5bfa1df80f5e829344058b444c7
Nicole Griffith <noteworthynicole@users.noreply.github.com>
2019-06-03 09:23:05 -0700

    placeholder test, DatabaseWrite, make test

--- src/backend/test/TestDatabaseWrite.java
+++ src/backend/test/TestDatabaseWrite.java
@@ -1,6 +1,14 @@
 package test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TestDatabaseWrite {
 	
+	@Test
+	public void testClassStringNoPreReq() {
+		assertTrue(true);
+	}
 
 }
