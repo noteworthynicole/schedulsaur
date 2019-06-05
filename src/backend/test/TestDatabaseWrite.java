743ee18ecbacd5b7da307603adb7340f43750de8
Thomas Bergmann <tommy.bergmann2332@gmail.com>
2019-06-04 22:23:47 -0700

    more smells

--- src/backend/test/TestDatabaseWrite.java
+++ src/backend/test/TestDatabaseWrite.java
@@ -7,7 +7,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
